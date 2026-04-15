6684ef7319887880e98baced46e4f5c58030e5d1
Perry Smith <pedz@easesoftware.com>
2026-04-15 10:13:12 -0500

    Emacs with eglot in an Xcode swift project now works
    Use `xcode-build-server config -project *.xcodeproj` in the project
    directory to set up `buildServer.json`.

--- customize.el
+++ customize.el
@@ -8,28 +8,31 @@
  '(Info-additional-directory-list '("/opt/homebrew/share/info"))
  '(Info-default-directory-list
    (delq nil
-         (mapcar
-          (lambda
-            (x)
-            (and
-             (file-executable-p x)
-             x))
-          Info-default-directory-list)))
+         (mapcar (lambda (x) (and (file-executable-p x) x))
+                 Info-default-directory-list)))
  '(ac-modes
-   '(emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode tcl-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode scss-mode less-css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode web-mode ts-mode sclang-mode verilog-mode qml-mode apples-mode))
+   '(emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode
+                     go-mode java-mode malabar-mode clojure-mode
+                     clojurescript-mode scala-mode scheme-mode
+                     ocaml-mode tuareg-mode coq-mode haskell-mode
+                     agda-mode agda2-mode perl-mode cperl-mode
+                     python-mode ruby-mode lua-mode tcl-mode
+                     ecmascript-mode javascript-mode js-mode js2-mode
+                     php-mode css-mode scss-mode less-css-mode
+                     makefile-mode sh-mode fortran-mode f90-mode
+                     ada-mode xml-mode sgml-mode web-mode ts-mode
+                     sclang-mode verilog-mode qml-mode apples-mode))
  '(ansi-color-for-comint-mode t)
  '(ansi-color-names-vector
-   ["black" "red" "green" "gold" "blue" "magenta" "darkturquoise" "dark green"])
+   ["black" "red" "green" "gold" "blue" "magenta" "darkturquoise"
+    "dark green"])
  '(backup-by-copying t)
  '(backup-by-copying-when-linked t)
  '(backup-directory-alist '(("." . "~/.config/emacs/backup")))
  '(case-fold-search nil)
  '(default-frame-alist
-   '((minibuffer . t)
-     (menu-bar-lines . 0)
-     (background-color . "black")
-     (cursor-color . "SlateBlue")
-     (foreground-color . "grey")
+   '((minibuffer . t) (menu-bar-lines . 0) (background-color . "black")
+     (cursor-color . "SlateBlue") (foreground-color . "grey")
      (mouse-color . "Blue")))
  '(delete-old-versions t)
  '(dired-use-ls-dired nil)
@@ -49,40 +52,22 @@
  '(initial-frame-alist
    (cond
     ((and
-      (or
-       (=
-        (display-pixel-height)
-        2490)
-       (=
-        (display-pixel-height)
-        2340)
-       (=
-        (display-pixel-height)
-        1440))
-      (=
-       (display-pixel-width)
-       2560))
-     '((top . 23)
-       (left . 835)
-       (width . 132)
-       (height . 95)))
-    ((and
-      (=
-       (display-pixel-height)
-       1050)
-      (=
-       (display-pixel-width)
-       1680))
-     '((top . 23)
-       (left . 360)
-       (width . 132)
-       (height . 70)))))
+      (or (= (display-pixel-height) 2490)
+          (= (display-pixel-height) 2340)
+          (= (display-pixel-height) 1440))
+      (= (display-pixel-width) 2560))
+     '((top . 23) (left . 835) (width . 132) (height . 95)))
+    ((and (= (display-pixel-height) 1050)
+          (= (display-pixel-width) 1680))
+     '((top . 23) (left . 360) (width . 132) (height . 70)))))
  '(js-indent-level 2)
  '(js2-global-externs '("jQuery" "$"))
  '(js2-include-gears-externs nil)
  '(js2-include-rhino-externs nil)
  '(lsp-disabled-clients '(typeprof-ls))
  '(lsp-enable-snippet nil)
+ '(lsp-log-io nil)
+ '(lsp-server-trace nil)
  '(lua-indent-level 2)
  '(mac-emulate-three-button-mouse t)
  '(mail-default-reply-to "pedz@easesoftware.com")
